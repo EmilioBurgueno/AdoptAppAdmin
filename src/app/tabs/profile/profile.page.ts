abd94019c9d6742f92c1e4501b14d0f1124cc6b5
JJAscencio <jhon0293@gmail.com>
2020-05-03 20:08:12 -0700

    Routing setup

---
+++ src/app/tabs/profile/profile.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+
+  constructor(private authService: AuthService,
+              private navCtrl: NavController,
+              private alertCtrl: AlertController) { }
+
+  ngOnInit() {
+  }
+
+
+  async deleteAccountAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Delete Account',
+      message: 'Do you really want to delete your account?',
+      buttons: [
+        {
+          text:'No',
+
+        },
+        {
+          text: 'Yes'
+        }]
+    });
+
+    await alert.present();
+  }
+
+  logout(): void {
+    this.authService.logout().then(() => {
+      this.navCtrl.navigateRoot(['']);
+    });
+  }
+
+}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,12 +4,17 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+          }
+        ]
       },
       {
         path: 'tab2',
