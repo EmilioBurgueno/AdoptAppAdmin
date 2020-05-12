8d13c5a822d193c26f21c6eb247a0cbaecd20aa6
JJAscencio <jhon0293@gmail.com>
2020-05-12 14:34:40 -0700

    Dog model

---
+++ src/models/dog.model.ts
@@ -0,0 +1,12 @@
+export interface Dog {
+    id: String,
+    dname: String,
+    size: String,
+    age: String,
+    fplace: String,
+    dfound: String,
+    description: String,
+    collar: String,
+    status: String,
+    profilepic: String
+}
