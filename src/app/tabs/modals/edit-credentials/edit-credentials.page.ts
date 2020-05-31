f61986a07bb77e4296733ae3c43ee9c87aedb0cb
isaacsanchez18 <luke_shakz@hotmail.com>
2020-05-31 16:11:20 -0700

    Condicion email repetido updateEmail()

--- src/app/tabs/modals/edit-credentials/edit-credentials.page.ts
+++ src/app/tabs/modals/edit-credentials/edit-credentials.page.ts
@@ -50,16 +50,21 @@ export class EditCredentialsPage implements OnInit {
   async updateEmail() {
     await this.presentLoading('Haciendo cambios...');
     if (this.editUserForm.valid) {
-      await this.authService.reauthenticate(this.editUserForm.controls.cPassword.value).then(() => {
+      if (this.editUserForm.controls.email.value === this.user.email) {
+        this.dismissLoading();
+        this.presentAlert('¡Error!', 'Este email ya existe. Ingresa uno nuevo.');
+      } else {
+        await this.authService.reauthenticate(this.editUserForm.controls.cPassword.value).then(() => {
 
           this.authService.changeEmail(this.editUserForm.controls.cPassword.value, this.editUserForm.controls.email.value);
           this.dismissLoading();
           this.presentAlertConfirm('¡Exito!', 'Tu email ha sido cambiado correctamente.');
           this.closeModal();
-      }).catch((error) => {
-        this.dismissLoading();
-        this.presentAlert('¡Lo sentimos!', 'Ha ocurrido un error.');
-      });
+        }).catch((error) => {
+          this.dismissLoading();
+          this.presentAlert('¡Error!', 'Tu contraseña no es correcta.');
+        });
+      }
     } else {
       this.dismissLoading();
       this.presentAlert('¡Error!', 'Tu contraseña actual no es correcta.');
